4396e8b395de89d8118f6295d7a5a7deb6cf77ce
Faiz Hannan <ananlol156@gmail.com>
2026-07-17 09:59:49 +0700

    update ui market

--- src/lib/page/market/HeadingResult.svelte
+++ src/lib/page/market/HeadingResult.svelte
@@ -6,25 +6,25 @@
 </script>
 
 <div class="w-full h-[10rem] mt-29 grid grid-rows-[12%_30%_2%_33%_5%_18%]">
-    <div class="bg-slate-400 text-sm">
-        <p class="{Full}">
-            search result
+    <div class="text-sm">
+        <p class="{Full} p-2 text-xs/loose">
+            SEARCH RESULT
         </p>
     </div>
 
 
 
-    <div class="bg-red-500">
-        <h1>nama search</h1>
+    <div class=" p-2 font-bold font-sans text-2xl">
+        <h1>BARANG 1</h1>
     </div>
 
 
     <div><!-- Hanya sebagai jarak --></div>
 
 
-    <div class="bg-blue-500 flex items-start space-x-3 p-2">
+    <div class="flex items-start space-x-3 p-2">
         {#each section as s, i}
-            <button class="p-1.5 text-slate-800/60 text-lg/tight border border-slate-900/30">{s}</button>
+            <button class=" text-slate-800/60 text-xs/loose border border-slate-900/30 pl-1 pr-1 hover:border-white hover:bg-slate-800 hover:text-white transition duration-300 ">{s}</button>
         {/each}
     </div>
 
@@ -32,8 +32,51 @@
     <div><!-- Hanya sebagai jarak --></div>
 
     
-    <div class="bg-teal-800 grid grid-cols-2">
-        <dir>Urut berdasar</dir>
-        <div class="text-right">Semua Filter</div>
+   <div class="p-2 grid grid-cols-2 items-center text-xs text-slate-800/70">
+
+    <div class="flex items-center gap-2">
+        <span class="tracking-wide">
+            URUTKAN BERDASAR
+        </span>
+
+        <svg 
+            class="w-4 h-4"
+            viewBox="0 0 24 24" 
+            fill="none" 
+            xmlns="http://www.w3.org/2000/svg">
+
+            <path fill-rule="evenodd" clip-rule="evenodd" 
+            d="M12 4.875C12.6213 4.875 13.125 5.37868 13.125 6V10.8752V13.1252V18.0007C13.125 18.622 12.6213 19.1257 12 19.1257C11.3787 19.1257 10.875 18.622 10.875 18.0007V13.1252V10.8752V6C10.875 5.37868 11.3787 4.875 12 4.875Z" 
+            fill="currentColor"/>
+
+            <g opacity="0.4">
+                <path d="M18.0007 10.875H13.1252V13.125H18.0007C18.622 13.125 19.1257 12.6213 19.1257 12C19.1257 11.3787 18.622 10.875 18.0007 10.875Z" fill="currentColor"/>
+                <path d="M6 13.125H10.8752V10.875H6C5.37868 10.875 4.875 11.3787 4.875 12C4.875 12.6213 5.37868 13.125 6 13.125Z" fill="currentColor"/>
+            </g>
+
+        </svg>
     </div>
+
+
+    <div class="flex items-center justify-end gap-2">
+        <svg 
+            class="w-4 h-4"
+            viewBox="0 0 25 24" 
+            fill="none" 
+            xmlns="http://www.w3.org/2000/svg">
+
+            <path d="M3.38501 9.75C3.38501 9.12868 3.88869 8.625 4.51001 8.625L20.51 8.625C21.1313 8.625 21.635 9.12868 21.635 9.75C21.635 10.3713 21.1313 10.875 20.51 10.875L4.51001 10.875C3.88869 10.875 3.38501 10.3713 3.38501 9.75Z" fill="currentColor"/>
+
+            <g opacity="0.4">
+                <path d="M18.01 13.125C18.6313 13.125 19.135 13.6287 19.135 14.25C19.135 14.8713 18.6313 15.375 18.01 15.375H7.01001C6.38869 15.375 5.88501 14.8713 5.88501 14.25C5.88501 13.6287 6.38869 13.125 7.01001 13.125H18.01Z" fill="currentColor"/>
+            </g>
+
+        </svg>
+
+        <span class="tracking-wide">
+            FILTER
+        </span>
+    </div>
+
+</div>
 </div>
--- src/lib/page/market/SearchingResult.svelte
+++ src/lib/page/market/SearchingResult.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
-
+    const ulang:number = 24
+    let ke: number = 0
+    function tambah(){
+        ke++
+    }
 </script>
 
 {#snippet ProductCard()}
-    <div class="grid grid-cols-2 h-[13rem] w-[19rem] rounded-sm">
-        <div class="relative bg-slate-400/25 rounded-l-sm overflow-hidden w-full h-full">
+    <div class="grid grid-cols-2 h-[15rem] w-[25.5rem] rounded-sm border border-zinc-800/20">
+        <div class="relative bg-zinc-400/15 rounded-l-sm overflow-hidden w-full h-full">
   <!-- Foto di paling belakang -->
-        <img src="https://images.unsplash.com/photo-1542291026-7eec264c27ff" alt="Product" class="absolute inset-0 w-full h-full object-cover z-0" />
+        <img src="src\constant\hm-removebg-preview.png" alt="Product" class="absolute inset-0 w-full h-full object-cover z-0" />
         
         <!-- Icon love di kanan atas melayang (ukuran menggunakan persen) -->
         <button class="absolute top-[3%] right-[3%] z-10 bg-white shadow-md text-slate-500 hover:scale-110 transition rounded-full flex items-center justify-center w-[12%] aspect-square">
@@ -15,24 +19,84 @@
         </svg>
         </button>
         <!-- Harga di bawah melayang (ukuran menggunakan persen) -->
-        <div class="absolute bottom-[3%] left-[3%] right-[3%] z-10  rounded-md flex flex-col justify-center px-[5%] h-[18%]">
+        <div class="absolute bottom-[-1.7%] left-[3%] right-[3%] z-10  rounded-md flex flex-col justify-center px-[2%] h-[18%]">
             <p class="text-sm font-bold text-slate-900">Rp 150.000</p>
         </div>
         </div>
-        <div class="bg-slate-800 rounded-r-sm p-2 grid grid-rows-[13%_22%_15%_32%_18%]">
-            <div class="text-white">Nama seller</div>
-            <div class="text-white">Nama barang</div>
-            <div class="text-white">deskripsi</div>
-            <div class="text-white">badge</div>
-            <div class="text-white">tambah keranjang</div>
+        <div class="rounded-r-sm p-2 grid grid-rows-[13%_15%_19%_35%_18%]">
+
+            <div class="text-slate-900/50 text-[10px] p-2 uppercase tracking-[0.18em] font-medium">
+                Nama seller
+            </div>
+
+            <div class="text-slate-800 text-lg pl-2 pt-1 font-semibold tracking-[-0.02em]">
+                Nama barang
+            </div>
+
+            <div class="min-h-0 text-slate-700/70 text-[10px] pl-2 pt-1.5 leading-relaxed font-light tracking-wide line-clamp-2 ">
+                Tas kulit premium dengan bahan berkualitas tinggi, desain minimalis modern cocok digunakan untuk berbagai aktivitas harian, memiliki ruang penyimpanan luas dan nyaman dipakai sepanjang hari.
+            </div>
+
+
+            <div class="text-slate-900 text-[10px] grid grid-cols-3 font-medium items-center pr-1 tracking-wide">
+
+                <div class="flex items-center justify-center gap-1">
+                    <span class="text-slate-700">★</span>
+                    <span>4.8</span>
+                </div>
+
+                <div class="flex justify-center text-slate-700/80">
+                    2.1k terjual
+                </div>
+
+                <div class="flex justify-center text-slate-700/80">
+                    Stok 12
+                </div>
+
+            </div>
+
+
+           <button class="group flex items-center justify-center gap-2 
+            text-slate-800/70 text-xs 
+            border border-slate-900/20 
+            px-3 py-1.5
+            hover:border-slate-900/50 
+            hover:bg-slate-900 
+            hover:text-white 
+            transition duration-300 
+            rounded-lg">
+
+                <span class="tracking-wide">
+                    TAMBAH KERANJANG
+                </span>
+
+                  <span class="opacity-80 transition duration-300">
+                        <svg 
+                            class="w-[15px] h-[15px]"
+                            viewBox="0 0 24 24" 
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+
+                            <path 
+                            fill-rule="evenodd" 
+                            clip-rule="evenodd" 
+                            d="M1.56641 4C1.56641 3.58579 1.90219 3.25 2.31641 3.25H3.49696C4.61854 3.25 5.56885 4.07602 5.72504 5.18668L5.7862 5.62161H19.7507C21.3714 5.62161 22.4605 7.28344 21.8137 8.76953L19.1464 14.8979C18.789 15.719 17.9788 16.25 17.0833 16.25L7.72179 16.25C6.60021 16.25 5.6499 15.424 5.49371 14.3133L4.23965 5.39556C4.18759 5.02534 3.87082 4.75 3.49696 4.75H2.31641C1.90219 4.75 1.56641 4.41421 1.56641 4Z"
+                            fill="currentColor"/>
+
+                        </svg>
+                    </span>
+
+            </button>
+
             <div></div>
+
         </div>
     </div>
 {/snippet}
 
-<div class="ml-2 w-full flex flex-wrap items-start space-y-1 gap-1 min-h-screen">
-    {@render ProductCard()}
-    {@render ProductCard()}
-    {@render ProductCard()}
-    {@render ProductCard()}
+<div class="pr-2 pl-2 w-full flex flex-wrap items-start gap-x-1 gap-y-[10px] min-h-screen">
+        {#each Array(ulang) as _, i}
+            {@render ProductCard()}
+        {/each}
+
 </div>
